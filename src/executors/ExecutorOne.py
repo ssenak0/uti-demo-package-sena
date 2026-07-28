e477ed94f1f945942df7373b5e50ea540148077c
Sena <sena250817@gmail.com>
2026-07-28 14:05:02 +0300

    chore: add try-except block to capture deployment errors

--- src/executors/ExecutorOne.py
+++ src/executors/ExecutorOne.py
@@ -12,17 +12,23 @@ from novavision.demo_package.utils.response import build_response_one
 
 class ExecutorOne(Component):
     def __init__(self, request, bootstrap):
-        super().__init__(request, bootstrap)
-        self.request.model = PackageModel(**(self.request.data))
-        self.image_one = self.request.get_param("inputImageOne")
-        self.method = self.request.get_param("demoDependentDropdown")
-        
-        if self.method == "OptionA":
-            self.val_int = self.request.get_param("optionAIntegerField")
-            self.val_bool = self.request.get_param("optionABoolField")
-        else:
-            self.val_float = self.request.get_param("optionBFloatField")
-            self.val_string = self.request.get_param("optionBStringField")
+        try:
+            super().__init__(request, bootstrap)
+            self.request.model = PackageModel(**(self.request.data))
+            self.image_one = self.request.get_param("inputImageOne")
+            self.method = self.request.get_param("demoDependentDropdown")
+            
+            if self.method == "OptionA":
+                self.val_int = self.request.get_param("optionAIntegerField")
+                self.val_bool = self.request.get_param("optionABoolField")
+            else:
+                self.val_float = self.request.get_param("optionBFloatField")
+                self.val_string = self.request.get_param("optionBStringField")
+        except Exception as e:
+            import traceback
+            with open("/tmp/executor_error.log", "w") as f:
+                f.write(traceback.format_exc())
+            raise
 
     @staticmethod
     def bootstrap(config: dict) -> dict:
